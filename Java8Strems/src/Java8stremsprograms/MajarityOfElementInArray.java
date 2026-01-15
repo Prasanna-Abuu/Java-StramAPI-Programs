5d1e8a1bf0f7d10fa98f7ee2de42a078afc85d98
Prasanna Kumar Thatha <118557000+Prasanna-Abuu@users.noreply.github.com>
2026-01-15 09:56:02 +0530

    Update MajarityOfElementInArray.java

--- Java8Strems/src/Java8stremsprograms/MajarityOfElementInArray.java
+++ Java8Strems/src/Java8stremsprograms/MajarityOfElementInArray.java
@@ -1,3 +1,48 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityOfElementInArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 2, 1, 2, 3, 2, 2};
+        int n = arr.length;
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        // Count frequency
+        for (int num : arr) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Find majority element
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() > n / 2) {
+                System.out.println("Majority Element : " + entry.getKey());
+                return;
+            }
+        }
+
+        System.out.println("No Majority Element Found");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------
+
+
 package Java8stremsprograms;
 
 
@@ -25,3 +70,4 @@ public static int majarityOfElement(int[] nums) {
 return candidate;
 }	
 }
+
